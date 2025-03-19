5d8d25a54c8101e0102f9f128dc2409d0d3be1dc
Carlos Leonel <carlos.leonel268@gmail.com>
2025-03-18 22:49:05 -0300

    Update verificar_num.py
    Tirei um print() que era para teste de código

--- Exercicios_py/verificar_num.py
+++ Exercicios_py/verificar_num.py
@@ -2,7 +2,6 @@ def verificar_numero(numero):
     if numero < 2:
         return False
     for num in range(2, int(numero ** 0.5) + 1):
-        print(num)
         if numero % num == 0:
             return False
     return True
