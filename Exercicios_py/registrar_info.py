a0219ce9a710136b3c76b5183855a83152eaf34f
Carlos Leonel <carlos.leonel268@gmail.com>
2025-03-18 22:59:27 -0300

    Update registrar_info.py

--- Exercicios_py/registrar_info.py
+++ Exercicios_py/registrar_info.py
@@ -1,10 +1,10 @@
 def registrar_informacoes(*args, **kwargs):
-    print("Escreva numeros")
+    print("Argumentos (args):")
     for arg in args:
         print(arg)
     
-    print("Informe sua nome, idade e seu pais")
+    print("Informacoes extras (kwargs):")
     for chave, valor in kwargs.items():
         print(f"{chave}: {valor}")
 
-registrar_informacoes(2, 4, 6, nome="Carlos", idade=20, pais= "Brasil")
+registrar_informacoes(2, 4, 6, nome="Carlos", idade=20, pais="Brasil")
